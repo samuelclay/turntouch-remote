9af51a448da831012733d1a354294fee68bbaa7b
Samuel Clay <samuel@ofbrooklyn.com>
2015-08-24 19:55:40 -0700

    Adding bootloader from nrf51sdk examples.

--- firmware/nrf51/libs/components/libraries/bootloader_dfu/bootloader_util.c
+++ firmware/nrf51/libs/components/libraries/bootloader_dfu/bootloader_util.c
@@ -69,9 +69,9 @@ isr_abort
 }
 #elif defined ( __GNUC__ )
 
-__attribute__ ((section(".bootloaderSettings"))) uint8_t m_boot_settings[1024] ;
-__attribute__ ((section(".uicrBootStartAddress"))) uint32_t m_uicr_bootloader_start_address = BOOTLOADER_REGION_START;
-const bootloader_settings_t const * const mp_bootloader_settings = (bootloader_settings_t *) &m_boot_settings[0];   /**< Read only pointer to bootloader settings in flash. */
+// __attribute__ ((section(".bootloaderSettings"))) uint8_t m_boot_settings[1024] ;
+// __attribute__ ((section(".uicrBootStartAddress"))) uint32_t m_uicr_bootloader_start_address = BOOTLOADER_REGION_START;
+// const bootloader_settings_t const * const mp_bootloader_settings = (bootloader_settings_t *) &m_boot_settings[0];   /**< Read only pointer to bootloader settings in flash. */
 
 static inline void bootloader_util_reset(uint32_t start_addr)
 {
