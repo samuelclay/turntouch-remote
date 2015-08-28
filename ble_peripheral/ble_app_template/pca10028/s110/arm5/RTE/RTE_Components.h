36b414e0012d4cfaeaf38caae9d4b26fcd84a7f0
Samuel Clay <samuel@ofbrooklyn.com>
2015-08-28 11:53:44 -0700

    Adding ble template for Keil.

---
+++ ble_peripheral/ble_app_template/pca10028/s110/arm5/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ble_app_template_s110_pca10028' 
+ * Target:  'nrf51422_xxac_s110' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define GPIOTE_CONFIG_NUM_OF_LOW_POWER_EVENTS=4>nrf_drv_gpiote
+#define GPIOTE_ENABLED=1>.
+  #define GPIOTE_CONFIG_NUM_OF_LOW_POWER_EVENTS=1>.
+#define S110
+#define SOFTDEVICE_PRESENT
+#define SWI_DISABLE0
+
+#endif /* RTE_COMPONENTS_H */
