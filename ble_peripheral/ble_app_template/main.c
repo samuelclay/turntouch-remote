425e100cca25b35a6dde0926345c813ab03c56cb
Samuel Clay <samuel@ofbrooklyn.com>
2015-09-28 18:06:52 -0700

    Adding BANK_VALID_APP to Keil app. Had to edit Keil's .pdsc file to include a config=true to the bootloader_settings.c file to unlock it.

--- ble_peripheral/ble_app_template/main.c
+++ ble_peripheral/ble_app_template/main.c
@@ -132,6 +132,9 @@ void bsp_evt_handler(bsp_event_t evt) {
             if (err_code == BLE_ERROR_GATTS_SYS_ATTR_MISSING) {
                 // Can ignore this error, just means that bluetooth is connected but nobody's listening yet
                 rtt_print(0, "BLE_ERROR_GATTS_SYS_ATTR_MISSING\n");
+            } else if (err_code == BLE_ERROR_NO_TX_BUFFERS) {
+                // Can ignore this error, just means buffer capacity exceeded (too many button pushes)
+                rtt_print(0, "BLE_ERROR_NO_TX_BUFFERS\n");
             } else {
                 APP_ERROR_CHECK(err_code);
             }
@@ -368,8 +371,8 @@ static void ble_stack_init(void)
     uint32_t err_code;
 
     // Initialize the SoftDevice handler module.
-    SOFTDEVICE_HANDLER_INIT(NRF_CLOCK_LFCLKSRC_XTAL_500_PPM, NULL);
-    //SOFTDEVICE_HANDLER_INIT(NRF_CLOCK_LFCLKSRC_RC_250_PPM_4000MS_CALIBRATION, NULL);
+    // SOFTDEVICE_HANDLER_INIT(NRF_CLOCK_LFCLKSRC_XTAL_20_PPM, NULL);
+    SOFTDEVICE_HANDLER_INIT(NRF_CLOCK_LFCLKSRC_RC_250_PPM_4000MS_CALIBRATION, NULL);
 
     // Enable BLE stack 
     ble_enable_params_t ble_enable_params;
