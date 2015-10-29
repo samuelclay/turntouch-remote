50a9fad9546102766627430cbe090216398f0104
Samuel Clay <samuel@ofbrooklyn.com>
2015-10-29 16:14:19 -0700

    Rearranging firmware.

--- firmware/src/main.h
+++ firmware/src/main.h
@@ -64,6 +64,10 @@
 #define APP_SERVICE_HANDLE_START        0x000C                                      /**< Handle of first application specific service when when service changed characteristic is present. */
 #define BLE_HANDLE_MAX                  0xFFFF                                      /**< Max handle value in BLE. */
 
+#define ADC_REF_VOLTAGE_IN_MILLIVOLTS        1200                                      /**< Reference voltage (in milli volts) used by ADC while doing conversion. */
+#define ADC_PRE_SCALING_COMPENSATION         3                                         /**< The ADC is configured to use VDD with 1/3 prescaling as input. And hence the result of conversion is to be multiplied by 3 to get the actual value of the battery voltage.*/
+#define DIODE_FWD_VOLT_DROP_MILLIVOLTS       270                                       /**< Typical forward voltage drop of the diode (Part no: SD103ATW-7-F) that is connected in series with the voltage supply. This is the voltage drop when the forward current is 1mA. Source: Data sheet of 'SURFACE MOUNT SCHOTTKY BARRIER DIODE ARRAY' available at www.diodes.com. */
+
 STATIC_ASSERT(IS_SRVC_CHANGED_CHARACT_PRESENT);                                     /** When having DFU Service support in application the Service Changed Characteristic should always be present. */
 
 static ble_gap_sec_params_t             m_sec_params;                               /**< Security requirements for this application. */
